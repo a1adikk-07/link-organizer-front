bfd6da1e666ac43a219269030d73f714ce1450bb
a1adikk-07 <136816822+a1adikk-07@users.noreply.github.com>
2024-09-20 00:50:55 +0200

    fixbug

--- src/components/AllLinkCards/AllLinkCards.jsx
+++ src/components/AllLinkCards/AllLinkCards.jsx
@@ -1,77 +1,98 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
-import { selectAllLinkCards } from '../../redux/linkCards/cards-selectors';
+import { useEffect } from 'react';
+
+import { selectFilteredCards } from '../../redux/linkCards/cards-selectors';
+
+import LinkCardsList from 'components/MyLinkCards/LinkCardsList/LinkCardsList';
+
+import {
+  fetchLinkCards,
+  deleteLinkCard,
+} from '../../redux/linkCards/link-cards-operations';
 
 import styles from './all-link-cards.module.css';
 
 const AllLinkCards = () => {
-  const items = useSelector(selectAllLinkCards);
+  const items = useSelector(selectFilteredCards);
 
-  const elements = items.map(
-    ({
-      id,
-      title,
-      firstName,
-      oneLink,
-      secondName,
-      twoLink,
-      thirdName,
-      threeLink,
-      fourthName,
-      fourLink,
-    }) => (
-      <li key={id} className={styles.card}>
-        <div className={styles.cardsTitleDiv}>
-          <h3 className={styles.cardsTitle}>{title}</h3>
-        </div>
-        <ul className={styles.cardsList}>
-          <li>
-            <a
-              href={oneLink}
-              target="_blank"
-              rel="noreferrer noopener"
-              className={styles.link}
-            >
-              {firstName}
-            </a>
-          </li>
-          <li>
-            <a
-              href={twoLink}
-              target="_blank"
-              rel="noreferrer noopener"
-              className={styles.link}
-            >
-              {secondName}
-            </a>
-          </li>
-          <li>
-            <a
-              href={threeLink}
-              target="_blank"
-              rel="noreferrer noopener"
-              className={styles.link}
-            >
-              {thirdName}
-            </a>
-          </li>
-          <li>
-            <a
-              href={fourLink}
-              target="_blank"
-              rel="noreferrer noopener"
-              className={styles.link}
-            >
-              {fourthName}
-            </a>
-          </li>
-        </ul>
-      </li>
-    )
-  );
+  const dispatch = useDispatch();
+
+  // const elements = items.map(
+  //   ({
+  //     id,
+  //     title,
+  //     firstName,
+  //     oneLink,
+  //     secondName,
+  //     twoLink,
+  //     thirdName,
+  //     threeLink,
+  //     fourthName,
+  //     fourLink,
+  //   }) => (
+  //     <li key={id} className={styles.card}>
+  //       <div className={styles.cardsTitleDiv}>
+  //         <h3 className={styles.cardsTitle}>{title}</h3>
+  //       </div>
+  //       <ul className={styles.cardsList}>
+  //         <li>
+  //           <a
+  //             href={oneLink}
+  //             target="_blank"
+  //             rel="noreferrer noopener"
+  //             className={styles.link}
+  //           >
+  //             {firstName}
+  //           </a>
+  //         </li>
+  //         <li>
+  //           <a
+  //             href={twoLink}
+  //             target="_blank"
+  //             rel="noreferrer noopener"
+  //             className={styles.link}
+  //           >
+  //             {secondName}
+  //           </a>
+  //         </li>
+  //         <li>
+  //           <a
+  //             href={threeLink}
+  //             target="_blank"
+  //             rel="noreferrer noopener"
+  //             className={styles.link}
+  //           >
+  //             {thirdName}
+  //           </a>
+  //         </li>
+  //         <li>
+  //           <a
+  //             href={fourLink}
+  //             target="_blank"
+  //             rel="noreferrer noopener"
+  //             className={styles.link}
+  //           >
+  //             {fourthName}
+  //           </a>
+  //         </li>
+  //       </ul>
+  //     </li>
+  //   )
+
+  useEffect(() => {
+    dispatch(fetchLinkCards());
+  }, [dispatch]);
+
+  const onDeleteCard = id => {
+    dispatch(deleteLinkCard(id));
+  };
+
   return (
     <div className={styles.cardsListContainer}>
-      <ul className={styles.cardList}>{elements}</ul>
+      <LinkCardsList items={items} deleteCard={onDeleteCard} />
+
+      {/* <ul className={styles.cardList}>{elements}</ul> */}
     </div>
   );
 };
--- src/components/favoriteLinkCards/favoriteLinkCards.jsx
+++ src/components/favoriteLinkCards/favoriteLinkCards.jsx
@@ -1,4 +1,13 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { useEffect } from 'react';
+
+import {
+  fetchLinkCards,
+  deleteLinkCard,
+} from '../../redux/linkCards/link-cards-operations';
+
+import LinkCardsList from 'components/MyLinkCards/LinkCardsList/LinkCardsList';
 
 import { selectFavoriteLinkCards } from '../../redux/linkCards/cards-selectors';
 
@@ -7,71 +16,84 @@ import styles from './favoriteLinkCards.module.css';
 const FavoriteLinkCardsElement = () => {
   const items = useSelector(selectFavoriteLinkCards);
 
-  const elements = items.map(
-    ({
-      id,
-      title,
-      firstName,
-      oneLink,
-      secondName,
-      twoLink,
-      thirdName,
-      threeLink,
-      fourthName,
-      fourLink,
-    }) => (
-      <li key={id} className={styles.card}>
-        <div className={styles.cardsTitleDiv}>
-          <h3 className={styles.cardsTitle}>{title}</h3>
-        </div>
-        <ul className={styles.cardsList}>
-          <li>
-            <a
-              href={oneLink}
-              target="_blank"
-              rel="noreferrer noopener"
-              className={styles.link}
-            >
-              {firstName}
-            </a>
-          </li>
-          <li>
-            <a
-              href={twoLink}
-              target="_blank"
-              rel="noreferrer noopener"
-              className={styles.link}
-            >
-              {secondName}
-            </a>
-          </li>
-          <li>
-            <a
-              href={threeLink}
-              target="_blank"
-              rel="noreferrer noopener"
-              className={styles.link}
-            >
-              {thirdName}
-            </a>
-          </li>
-          <li>
-            <a
-              href={fourLink}
-              target="_blank"
-              rel="noreferrer noopener"
-              className={styles.link}
-            >
-              {fourthName}
-            </a>
-          </li>
-        </ul>
-      </li>
-    )
-  );
+  // const elements = items.map(
+  //   ({
+  //     id,
+  //     title,
+  //     firstName,
+  //     oneLink,
+  //     secondName,
+  //     twoLink,
+  //     thirdName,
+  //     threeLink,
+  //     fourthName,
+  //     fourLink,
+  //   }) => (
+  //     <li key={id} className={styles.card}>
+  //       <div className={styles.cardsTitleDiv}>
+  //         <h3 className={styles.cardsTitle}>{title}</h3>
+  //       </div>
+  //       <ul className={styles.cardsList}>
+  //         <li>
+  //           <a
+  //             href={oneLink}
+  //             target="_blank"
+  //             rel="noreferrer noopener"
+  //             className={styles.link}
+  //           >
+  //             {firstName}
+  //           </a>
+  //         </li>
+  //         <li>
+  //           <a
+  //             href={twoLink}
+  //             target="_blank"
+  //             rel="noreferrer noopener"
+  //             className={styles.link}
+  //           >
+  //             {secondName}
+  //           </a>
+  //         </li>
+  //         <li>
+  //           <a
+  //             href={threeLink}
+  //             target="_blank"
+  //             rel="noreferrer noopener"
+  //             className={styles.link}
+  //           >
+  //             {thirdName}
+  //           </a>
+  //         </li>
+  //         <li>
+  //           <a
+  //             href={fourLink}
+  //             target="_blank"
+  //             rel="noreferrer noopener"
+  //             className={styles.link}
+  //           >
+  //             {fourthName}
+  //           </a>
+  //         </li>
+  //       </ul>
+  //     </li>
+  //   )
+  // );
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchLinkCards());
+  }, [dispatch]);
+
+  const onDeleteCard = id => {
+    dispatch(deleteLinkCard(id));
+  };
+
   return (
     <div className={styles.cardsListContainer}>
-      <ul className={styles.cardList}>{elements}</ul>
+      <LinkCardsList items={items} deleteCard={onDeleteCard} />
+
+      {/* <ul className={styles.cardList}>{elements}</ul> */}
     </div>
   );
 };
